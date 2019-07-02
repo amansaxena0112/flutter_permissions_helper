7cd08c2416ba21a58725e9a14b0d83dc2dc68188
aman0112 <aman.saxena0112@gmail.com>
2019-07-02 13:11:35 +0530

    androidx compatibility

--- android/src/main/java/com/andrewackerman/flutterpermissionshelper/FlutterPermissionsHelperPlugin.java
+++ android/src/main/java/com/andrewackerman/flutterpermissionshelper/FlutterPermissionsHelperPlugin.java
@@ -7,11 +7,10 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 
-import java.security.Permission;
 import java.util.HashMap;
 
 import io.flutter.plugin.common.MethodCall;
